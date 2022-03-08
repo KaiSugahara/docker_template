0629d2acd5d0df504e4ccd1dd27a08134f399ea4
Kai Sugahara <kai@fulfills.jp>
2022-03-08 01:57:30 +0000

    Update: start as non-root user

--- wordpress/run.sh
+++ wordpress/run.sh
@@ -62,6 +62,8 @@ FROM wordpress:latest
 RUN groupmod -g ${YOUR_UID} www-data
 RUN usermod -u ${YOUR_UID} www-data
 RUN chown -R www-data:www-data /usr/src/wordpress
+
+USER www-data
 EOF
 
 # RUN CONTAINER(S)
