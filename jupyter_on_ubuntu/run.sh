ffbc4f85208e7a073fdb98f7e6bf1359dcdde942
Kai Sugahara <git@ksugahara.com>
2022-04-22 17:38:47 +0900

    Fix: 環境変数HTTPS_PROXYの設定

--- jupyter_on_ubuntu/run.sh
+++ jupyter_on_ubuntu/run.sh
@@ -26,6 +26,7 @@ services:
       context: .
       args:
         - HTTP_PROXY=${JUPYTER_HTTP_PROXY}
+        - HTTPS_PROXY=${JUPYTER_HTTP_PROXY}
         - YOUR_UID=${YOUR_UID}
         - YOUR_GID=${YOUR_GID}
         - CONTAINER_USER_NAME=${CONTAINER_USER_NAME}
