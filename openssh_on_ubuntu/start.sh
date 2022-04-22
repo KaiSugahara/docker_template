5aec7d3d54eb239439a71cf17966557ff608ed44
Kai Sugahara <git@ksugahara.com>
2022-04-22 13:35:47 +0900

    first commit

---
+++ openssh_on_ubuntu/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+
+export SHELL=/bin/bash
+
+su -c '/usr/sbin/sshd' << EOF
+password
+EOF
+
+# Jupyterの起動
+if type "jupyter" > /dev/null 2>&1; then
+    jupyter lab &
+fi
+
+# コンテナを起動し続る
+tail -f /dev/null
