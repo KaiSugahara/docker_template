775d19f82cf95fc29779ee4eec177a24e82f2f7e
Kai Sugahara <git@ksugahara.com>
2023-05-19 06:38:38 +0000

    Remove: nginx

--- mlflow_tracking/run.sh
+++ mlflow_tracking/run.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# EXPORT VARIABLE(S)
-read -p "VIRTUAL_HOST: " VIRTUAL_HOST
-read -p "LETSENCRYPT_EMAIL: " LETSENCRYPT_EMAIL
-
-export VIRTUAL_HOST LETSENCRYPT_EMAIL
-
 # RUN DOCKER-COMPOSE
 docker-compose build --no-cache
 docker-compose up -d
