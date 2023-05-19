c68b4064e25410b203c3ca462be7b04a8ee992d9
Kai Sugahara <git@ksugahara.com>
2023-05-19 07:20:51 +0000

    Update: 任意のプロジェクト名を指定可能に

--- mlflow_tracking/run.sh
+++ mlflow_tracking/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # EXPORT VARIABLE(S)
+read -p "PROJECT_NAME: " PROJECT_NAME
 read -p "TRACKING_PORT: " TRACKING_PORT
-export TRACKING_PORT
+export PROJECT_NAME, TRACKING_PORT
 
 # RUN DOCKER-COMPOSE
 docker-compose build --no-cache
-docker-compose up -d
+docker-compose -p ${PROJECT_NAME} up -d
