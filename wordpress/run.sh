69859b352d0e7bd4e90a1e93d0244d7e1bab7756
Kai Sugahara <git@ksugahara.com>
2023-05-22 14:04:57 +0000

    Update: 分離

--- wordpress/run.sh
+++ wordpress/run.sh
@@ -1,4 +1,8 @@
-# READ
+#!/bin/bash
+
+# EXPORT VARIABLE(S)
+export YOUR_UID=`id -u`
+
 echo "作成するコンテナ名を入力してください（入力例「wp-domain-name」）"
 read -p "> " CONTAINER_NAME
 
@@ -10,9 +14,6 @@ read -p "> " DOMAIN_NAME_LIST
 
 echo "証明書取得に利用するメールアドレスを入力してください"
 read -p "> " EMAIL_ADDRESS
-
-echo "あなたのUIDを入力してください"
-read -p "> " YOUR_UID
 
 echo "カレントディレクトリに public_html を作成し、公開ファイルのマウント先として良いですか？ [y/n]"
 read -p "> " WORDPRESS_PATH_FLAG
@@ -26,45 +27,7 @@ case $WORDPRESS_PATH_FLAG in
         ;;
 esac
 
-# EXPORT docker-compose.yml
-cat << EOF > docker-compose.yml
-version: '3'
-
-services:
-  ${CONTAINER_NAME}:
-    build:
-      context: .
-      dockerfile: Dockerfile
-    image: ${YOUR_UID}/wordpress:latest
-    container_name: ${CONTAINER_NAME}
-    volumes:
-      - ${WORDPRESS_PATH}:/var/www/html
-    restart: always
-    environment:
-      WORDPRESS_DB_HOST: db-mysql:3306
-      WORDPRESS_DB_NAME: ${DB_NAME}
-      WORDPRESS_DB_USER: wordpress
-      WORDPRESS_DB_PASSWORD: wordpress
-      VIRTUAL_HOST: ${DOMAIN_NAME_LIST}
-      LETSENCRYPT_HOST: ${DOMAIN_NAME_LIST}
-      LETSENCRYPT_EMAIL: ${EMAIL_ADDRESS}
-
-networks:
-  default:
-    external: true
-    name: nginx-network
-EOF
-
-# EXPORT Dockerfile
-cat << EOF > Dockerfile
-FROM wordpress:latest
-
-RUN groupmod -g ${YOUR_UID} www-data
-RUN usermod -u ${YOUR_UID} www-data
-RUN chown -R www-data:www-data /usr/src/wordpress
-EOF
+export YOUR_UID CONTAINER_NAME DB_NAME DOMAIN_NAME_LIST EMAIL_ADDRESS YOUR_UID WORDPRESS_PATH
 
 # RUN CONTAINER(S)
-docker-compose up -d
-rm docker-compose.yml
-rm Dockerfile
+docker-compose -p $CONTAINER_NAME up -d
