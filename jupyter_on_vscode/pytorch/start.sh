41c6481008734f62d0b4cbecb1cc18fa6448aa6b
Kai Sugahara <git@ksugahara.com>
2023-05-29 19:08:18 +0900

    Update:

--- jupyter_on_vscode/pytorch/start.sh
+++ jupyter_on_vscode/pytorch/start.sh
@@ -9,7 +9,7 @@ EOF
 
 # Jupyterの起動
 if type "jupyter" > /dev/null 2>&1; then
-    jupyter lab --ip="0.0.0.0" --no-browser --port=8888 &
+    jupyter lab --ip="0.0.0.0" --no-browser --port=8888 --ServerApp.password="" &
 fi
 
 # コンテナを起動し続る
