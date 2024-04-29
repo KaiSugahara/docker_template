74958aa43fc69c280502fa67956f4ba78527b999
Kai Sugahara <git@kais.jp>
2024-04-29 11:42:30 +0000

    Add:

---
+++ code-server/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+############################################################
+# Input container settings
+############################################################
+
+echo "1. 作成するコンテナ名を入力してください（例「yourname_ubuntu」）"
+read -p "> " CONTAINER_NAME
+
+echo "2. 作成するイメージ名を入力してください（例「yourname/ubuntu」）"
+read -p "> " IMAGE_NAME
+
+echo "3. code-serverの起動ポート番号を入力してください"
+read -p "> " CODE_PORT
+
+############################################################
+# Make config.yaml
+############################################################
+
+echo "bind-addr: 0.0.0.0:8080" > config.yaml
+echo "auth: none" >> config.yaml
+echo "cert: false" >> config.yaml
+
+############################################################
+# Make container
+############################################################
+
+# # EXPORT VARIABLE(S)
+export CONTAINER_NAME IMAGE_NAME CODE_PORT
+
+# DOCKER-COMPOSE
+docker-compose build --no-cache --pull
+docker-compose -p $CONTAINER_NAME up -d
